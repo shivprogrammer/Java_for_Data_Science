e5b1cc6333c43360d5cdc5fe254d5c0d3d7aa263
shivprogrammer <shiv.unadkat@gmail.com>
2018-02-26 00:55:20 -0800

    parsing the example json file for the first name property using Java

--- Chapter-01/01-The_Importance_and_Process_of_Cleaning_Data.java
+++ Chapter-01/01-The_Importance_and_Process_of_Cleaning_Data.java
@@ -5,3 +5,28 @@
 for (int i = 0; i < csvArray.length; i++) {
   tokenArray[i] = csvArray[i].split(",");
 }
+
+// The following is parsing information from 01-example.JSON
+
+// The code sequence that follows shows how to extract the values for fields of a person. A parser is created, which uses getCurrentName to retrieve a field name. If the name is firstname, then the getText method returns the value for that field. The other fields are handled in a similar manner.
+
+try {
+  JsonFactory jsonfactory = new JsonFactory();
+  JsonParser parser = jsonfactory.createParser(
+      new File("01-example.json"))
+  while (parser.nextToken() != JsonToken.END_OBJECT) {
+    String token = parser.getCurrentName();
+    if ("firstname".equals(token)) {
+        parser.nextToken();
+        String fname = parser.getText();
+        out.println("firstname : " + fname);
+    }
+    ...
+  }
+  parser.close();
+} catch (IOException ex) {
+  // Handle exceptions
+}
+
+// The output of this examle is as follows:
+// firstname : Smith
